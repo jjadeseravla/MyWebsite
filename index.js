9e70a610e271231a9a0279669f5721f958047f50
jjade seravla <jadealvares@hotmail.co.uk>
2018-08-20 12:23:26 +0100

    can console.log data inputted

--- index.js
+++ index.js
@@ -12,13 +12,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Set static path
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+// app.get('/', function(req, res){
+//   res.send('Hello World');
+// });
 
-app.get('/about', function(req, res, next) {
-  res.render('./dist/about.html');
-})
+// app.get('/about', function(req, res, next) {
+//   res.render('./dist/about.html');
+// });
+
+app.post('/users/add', function(req, res){
+  var newUser = {
+    full_name: req.body.full_name,
+    email: req.body.email,
+    query: req.body.query
+  }
+  console.log(newUser);
+});
 
 app.listen(3000, function() {
   console.log('Server started on port 3000...');
